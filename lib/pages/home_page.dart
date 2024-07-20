f88ed0727bd191ce9ba44524a9ec61c9ff54d6c0
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2024-07-20 20:48:12 +0530

    add list item complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:todo/utils/todo_List.dart';
 
+// ignore: must_be_immutable
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List toDoList = [
+    ['Learn Flutter', false],
+    ['Drink Cofee', false],
+    ['Drink Cofee', false],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.deepPurple.shade300,
+      appBar: AppBar(
+        title: const Text(
+          textAlign: TextAlign.center,
+          'Simple ToDo',
+        ),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return TodoList(
+              taskName: toDoList[index][0],
+            );
+          }),
     );
   }
 }
